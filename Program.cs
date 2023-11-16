2434556d4850b89732ddb23d8c7fe7d19b20ec6b
glsaacke <glsaacke@crimson.ua.edu>
2023-11-16 16:39:33 -0600

    11.15.23

--- Program.cs
+++ Program.cs
@@ -36,12 +36,27 @@ static void MenuLogic(string menuInput){
 
 static void ManagerialMenu(){
     System.Console.WriteLine("You are now in the managerial functions menu. Please select an option below:");
-    System.Console.WriteLine("");
-}
+    System.Console.WriteLine("1. Add a new ride to park inventory\n2. Remove a ride from park inventory\n3. Edit information about a ride\n4. Access report menu\n5. Return to home menu");
+    int check = 0;
+    while(check == 0){
+        try{
+            int userInput = int.Parse(Console.ReadLine());
+            check = 1;
+        }
+        catch{
+            Error();
+        }
+    }
+
+    if(userInput == 1){
+
+    }
+    
+}   
 
 static void CustomerMenu(){
     System.Console.WriteLine("You are now in the customer interface menu. Please select an option below");
-    System.Console.WriteLine("");
+    System.Console.WriteLine("1. View all operational rides\n2. Reserve a ride\n3. View ride history\n4. Update user account information\n5. Cancel a reservation\n6. Return to home menu");
 }
 
 static void Error(){
@@ -50,5 +65,3 @@ static void Error(){
     Console.ResetColor();
 }
 
-Ride one = new Ride();
-Ride.GetRideID(); 
